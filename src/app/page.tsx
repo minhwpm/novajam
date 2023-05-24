f2394d7a28db430dff886746044914073eba1514
Minh Nguyen <minhhien134@gmail.com>
2023-05-24 15:56:18 +0700

    FakeMessageBox UI component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,17 @@
 import Image from 'next/image'
 import FeatureRow from "@/components/sections/FeatureRow/FeatureRow"
 import Testimonials from '@/components/sections/Testimonials/Testimonials'
+import FakeMessageBox from '@/components/elements/FakeMessageBox/FakeMessageBox'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24">
+    // <main className="flex min-h-screen flex-col items-center justify-between py-24">
+    <main className="py-24">
+      <div className="mb-24 flex justify-center">
+        <div className="w-full md:w-[600px] h-[500px]">
+          <FakeMessageBox />
+        </div>
+      </div>
       <div className="px-4 lg:px-32 mb-24">
         <FeatureRow />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,42 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        pop: {
+          '0%': {
+            opacity: 0,
+            transform: 'scale(0.6)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'scale(1)',
+            visibility: 'visible',
+          },
+        },
+        fadeIn: {
+          '0%': {
+            opacity: 0,
+          },
+          '100%': {
+            opacity: 1,
+            visibility: 'visible',
+          }
+        },
+        slideUp: {
+          '0%': {
+            'top': '0'
+          },
+          '100%': {
+            'top': '-75%'
+          }
+        }
+
+      },
+      animation: {
+        pop: 'pop 0.3s ease-out forwards 1',
+        fadeIn: 'fadeIn 0.5s ease-in forwards 1',
+        slideUp: 'slideUp 0.3s ease-in forwards 1',
+      }
     },
   },
   plugins: [],
