758fa26977a6d6dd2dee929a8bc982195e2f4725
Minh Nguyen <minhhien134@gmail.com>
2023-05-29 16:54:16 +0700

    add Features page

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -4,10 +4,11 @@ import Hero from '@/components/sections/Hero/Hero'
 import ExpandingCTA from "@/components/sections/ExpandingCTA/ExpandingCTA"
 import FeaturesHero from "@/components/sections/FeaturesHero/FeaturesHero"
 
-export default function Home() {
+export default function Features() {
   return (
-    <main className="flex flex-col min-h-screen gap-20 pb-24">
-      <Hero />
+    <main className="flex flex-col min-h-screen gap-20 py-16">
+      {/* <Hero /> */}
+      <FeaturesHero />
       <FeatureRow />
       <Testimonials />
       <ExpandingCTA />
