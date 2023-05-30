feeec6b1e04934fd4aeb6030d6658ac3b6b48d31
Minh Nguyen <minhhien134@gmail.com>
2023-05-30 12:43:45 +0700

    tweak

--- src/components/elements/Button/Button.tsx
+++ src/components/elements/Button/Button.tsx
@@ -11,14 +11,14 @@ interface ButtonProps {
 }
 
 const Button = ({children, variant, url, size = "base", clickHandler}: ButtonProps) => {
-  const classes = classNames(classNames(
+  const classes = classNames(
     "rounded hover:animate-poop transition-all duration-500",
-    {"border-2" : variant === "border"},
+    {"border-2 border-black hover:bg-black hover:text-white" : variant === "border"},
     {"bg-blue-600 hover:bg-blue-700 text-white" : variant === "primary"},
     {"bg-orange-300 hover:bg-orange-400 hover:text-white" : variant === "secondary"},
     {"px-6 py-3": size === "base"},
     {"px-10 py-3 text-lg": size === "lg"},
-  ))
+  )
   if (url) {
     return (
       <Link href={url} className={classes}>
