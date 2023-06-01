e5bdebe2950ac383c1f94ae6edd9209eecfc5014
Minh Nguyen <minhhien134@gmail.com>
2023-06-01 14:50:51 +0700

    Timeline UI component for Mobile

--- src/components/sections/Timeline/Timeline.tsx
+++ src/components/sections/Timeline/Timeline.tsx
@@ -2,7 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import classNames from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCloudBolt, faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCloudBolt,
+  faBars,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 const dummyData = {
   title: "Make customer experience your competitive advantage",
@@ -55,37 +59,55 @@ interface Props {
 }
 
 const ContentItem = ({ section, idx, setVisibleIdx }: Props) => {
-  const divRef = useRef<HTMLDivElement>(null)
-  const [ active, setActive ] = useState(false)
+  const divRef = useRef<HTMLDivElement>(null);
+  const [active, setActive] = useState(false);
   useEffect(() => {
     function handler() {
       if (divRef.current != null) {
-        const box = divRef.current.getBoundingClientRect()
+        const box = divRef.current.getBoundingClientRect();
         if (box.y < window.innerHeight / 3) {
-          setActive(true)
-          setVisibleIdx(idx)
-        } else setActive(false)
+          setActive(true);
+          setVisibleIdx(idx);
+        } else setActive(false);
       }
     }
-    document.addEventListener('scroll', handler)
-    return () => document.removeEventListener('scroll', handler)
-  })
+    document.addEventListener("scroll", handler);
+    return () => document.removeEventListener("scroll", handler);
+  });
   return (
     <div
       ref={divRef}
       className={classNames(
-        "relative min-h-[50vh] px-10 transition-opacity duration-300 border-l-2 border-dashed",
+        "relative min-h-[50vh] px-10 pb-14 transition-opacity duration-300 border-l-2 border-dashed"
       )}
     >
+      {/* Milestone Circle */}
       <div className="bg-white absolute -left-1 top-2 z-20">
-        <div className={classNames(
-          "z-20 ml-[-3px] h-[12px] w-[12px] origin-center transform rounded-full border-[2px] transition-transform",
-          { " border-gray-200 bg-white group-hover:border-blue-600" : !active},
-          { "border-white bg-blue-600 ring-[2px] ring-blue-600" : active}
-        )} />
+        <div
+          className={classNames(
+            "z-20 ml-[-3px] h-[12px] w-[12px] origin-center transform rounded-full border-[2px] transition-transform",
+            {
+              " border-gray-200 bg-white group-hover:border-blue-600": !active,
+            },
+            { "border-white bg-blue-600 ring-[2px] ring-blue-600": active }
+          )}
+        />
       </div>
-      <h3 className={classNames("text-2xl font-semibold mb-3", { "text-blue-600": active})}>{section.content.title}</h3>
-      <p className="text-lg">{section.content.description}</p>
+      <h3
+        className={classNames("text-2xl font-semibold mb-3", {
+          "text-blue-600": active,
+        })}
+      >
+        {section.content.title}
+      </h3>
+      <p className="text-lg mb-8">{section.content.description}</p>
+      <Image
+        className="lg:hidden w-full object-cover"
+        src={section.media.src}
+        alt={section.content.title}
+        width={500}
+        height={500}
+      />
     </div>
   );
 };
@@ -93,10 +115,10 @@ const ContentItem = ({ section, idx, setVisibleIdx }: Props) => {
 const Timeline = ({ data = dummyData }) => {
   const { title, sections } = data;
   const sectionRef = useRef<HTMLDivElement>(null);
-  const [ visibleIdx, setVisibleIdx ] = useState(0);
-  const [ scrolled, setScrolled] = useState("not-yet")
+  const [visibleIdx, setVisibleIdx] = useState(0);
+  const [scrolled, setScrolled] = useState("not-yet");
   useEffect(() => {
-    function handler () {
+    function handler() {
       var winScroll =
         document.body.scrollTop || document.documentElement.scrollTop;
       if (sectionRef.current !== null) {
@@ -112,47 +134,59 @@ const Timeline = ({ data = dummyData }) => {
         var scrolled2 = ((winScroll - top + 250) / height2) * 100;
 
         if (scrolled2 <= 0) {
-          setScrolled("not-yet")
+          setScrolled("not-yet");
         }
         if (scrolled2 > 0 && scrolled2 < 100) {
-          setScrolled("scrolling")
+          setScrolled("scrolling");
         }
         if (scrolled2 >= 100) {
-          setScrolled("scrolled")
+          setScrolled("scrolled");
         }
       }
-    };
-    document.addEventListener('scroll', handler)
+    }
+    document.addEventListener("scroll", handler);
   }, [scrolled]);
   return (
-    <section  className="px-4 lg:px-32">
+    <section className="px-4 lg:px-32">
       <h2 className="text-4xl leading-snug lg:text-5xl lg:leading-snug text-center max-w-screen-lg font-semibold mb-16 mx-auto">
         {title}
       </h2>
       <div className="relative">
-        <div className="absolute left-[50%] ml-[-1px] mt-[-60px] hidden h-[60px] w-[2px] bg-gradient-to-b from-blue-600/0 to-blue-600/100 lg:block" />
-        <div className="absolute left-[20px] ml-[-8px] hidden h-full w-[16px] sm:left-[36px] lg:left-[50%] lg:block">
+        <div className="absolute left-0 lg:left-[50%] ml-[-1px] mt-[-60px] h-[60px] w-[2px] bg-gradient-to-b from-blue-600/0 to-blue-600/100 lg:block" />
+        <div className="absolute ml-[-8px] h-full w-[16px] sm:left-[36px] lg:left-[50%] lg:block">
           {/* Progress bar */}
           <div
             className={classNames(
-              "ml-[-1px] w-[2px] from-gray-100 bg-blue-600 <lg:hidden left-[50%] z-20 origin-bottom h-[250px] transition-transform duration-100",
-              { "hidden" : scrolled === "not-yet"},
-              { "fixed bottom-[67%]" : scrolled === "scrolling"},
-              { "absolute bottom-0" : scrolled === "scrolled"},
+              "ml-[-1px] w-[2px] from-gray-100 bg-blue-600 <lg:hidden lg:left-[50%] z-20 origin-bottom h-[250px] transition-transform duration-100",
+              { hidden: scrolled === "not-yet" },
+              { "fixed left-4 bottom-[67%]": scrolled === "scrolling" },
+              { "absolute left-2 bottom-0": scrolled === "scrolled" }
             )}
           />
           {/* Circle */}
-          <div className={classNames(
-            "flex justify-center items-center z-40 ml-[-14px] box-content h-[24px] w-[24px] transform rounded-full border-[2px] border-white bg-white ring-[2px] ring-blue-600 transition-transform duration-500 lg:left-[50%]",
-            {"absolute left-[-8px] top-0 sm:left-[-24px]]" : scrolled === "not-yet"},
-            {"fixed left-[20px] top-[33%] sm:left-[36px]" : scrolled === "scrolling"},
-            {"absolute bottom-0" : scrolled === "scrolled"}
-          )}>
-            <FontAwesomeIcon className="h-[20px] w-[20px] transform rounded-full transition duration-500 text-blue-600 bg-white" width={20} icon={faCloudBolt} />
+          <div
+            className={classNames(
+              "flex justify-center items-center lg:left-[50%] z-40 ml-[-14px] box-content h-[24px] w-[24px] transform rounded-full border-[2px] border-white bg-white ring-[2px] ring-blue-600 transition-transform duration-500",
+              {
+                "absolute top-0 left-[10px]":
+                  scrolled === "not-yet",
+              },
+              {
+                "fixed top-[33%] left-[17px]":
+                  scrolled === "scrolling",
+              },
+              { "absolute bottom-0 left-[10px]": scrolled === "scrolled" }
+            )}
+          >
+            <FontAwesomeIcon
+              className="h-[20px] w-[20px] transform rounded-full transition duration-500 text-blue-600 bg-white"
+              width={20}
+              icon={faCloudBolt}
+            />
           </div>
         </div>
         <div className="flex">
-          <div className="relative w-1/2">
+          <div className="relative hidden lg:block lg:w-1/2">
             <div className="sticky top-20">
               <div className="relative h-[80vh]">
                 {sections.map((section, idx) => (
@@ -182,7 +216,7 @@ const Timeline = ({ data = dummyData }) => {
               </div>
             </div>
           </div>
-          <div ref={sectionRef} className="w-1/2 flex flex-col">
+          <div ref={sectionRef} className="lg:w-1/2 flex flex-col">
             {sections.map((section, idx) => (
               <ContentItem
                 key={section.content.title}
