e2ca8406c71ffaf74ac18ed882eef134ab8820d5
Minh Nguyen <minhhien134@gmail.com>
2023-06-16 14:34:06 +0700

    AccordionPT

--- src/app/demos/saas/page.tsx
+++ src/app/demos/saas/page.tsx
@@ -1,13 +1,71 @@
+'use client'
+
 import ScrollingPresentation from "@/components/sections/ScrollingPresentation/ScrollingPresentation"
 import Testimonials from '@/components/sections/Testimonials/Testimonials'
 import Hero from '@/components/sections/Hero/Hero'
 import ExpandingCTA from "@/components/sections/ExpandingCTA/ExpandingCTA"
 import FeaturesHero from "@/components/sections/FeaturesHero/FeaturesHero"
+import AccordionPT from "@/components/celestial/AccordionPT/AccordionPT"
+import Section from "@/components/elements/Section/Section"
+
+const defaultPageData = {
+  sections: {
+    presentationSection: {
+      label: "",
+      title: "Chatbots can reduce your customer support team’s workload",
+      subtitle: "Reduce your customer support team’s duties, so your agents can focus on more complex issues.",
+      slides: [
+        {
+          label: "",
+          title: "Automate answers to repetitive questions",
+          subtitle: "",
+          content: "Include chatbots in your conversation to step in and handle the most repetitive actions like checking order status and processing returns.",
+          media: {
+            type: "image",
+            src: "https://bluebiz-assets.s3.ap-southeast-1.amazonaws.com/demos/saas/feature1.webp",
+          },
+          url: ""
+        },
+        {
+          label: "",
+          title: "Easily segment users",
+          subtitle: "",
+          content: "Easily group website visitors and customers based on their behavior to send them personalized messages.",
+          media: {
+            type: "image",
+            src: "https://bluebiz-assets.s3.ap-southeast-1.amazonaws.com/demos/saas/feature2.webp",
+          },
+          url: ""
+        },
+        {
+          label: "",
+          title: "Notify operators about specific actions",
+          subtitle: "",
+          content: "Notify operators when the customer closes the chat window, wants to leave the website, or wants to set up a conversation with a human agent.",
+          media: {
+            type: "image",
+            src: "https://bluebiz-assets.s3.ap-southeast-1.amazonaws.com/demos/saas/feature3.webp",
+          },
+          url: ""
+        }
+
+      ]
+    }
+  }
+}
 
 export default function Home() {
+
+  const { presentationSection } = defaultPageData.sections
   return (
     <main className="flex flex-col min-h-screen gap-20 pb-24">
-      <Hero />
+      <FeaturesHero />
+      <Section
+        title={presentationSection.title}
+        subtitle={presentationSection.subtitle}
+      >
+        <AccordionPT data={presentationSection.slides} />
+      </Section>
       <ScrollingPresentation />
       <Testimonials />
       <ExpandingCTA />
---
+++ src/components/celestial/AccordionPT/AccordionPT.tsx
@@ -0,0 +1,99 @@
+// Created by minhwpm (minhhien134@gmail.com)
+// AccordionPT = Accordion Presentation
+
+import React from 'react';
+import * as RadixAccordion from '@radix-ui/react-accordion';
+import GridBox from '../../elements/GridBox/GridBox';
+import classNames from "classnames";
+import Image from 'next/image';
+import "./styles.css"
+
+interface TabsProps {
+  data?: Array<{
+    label?: string
+    title: string
+    subtitle?: string
+    content: string
+    media?: {
+      type: string
+      src: string
+    }
+    url?: string
+  }>
+}
+
+const VTabs = ({data}: TabsProps) => {
+  const [ activeItem, setActiveItem ] = React.useState(data ? data[0].title : '')
+  if (data && data.length > 0) {
+    return (
+      <div className="grid grid-cols-12">
+        <RadixAccordion.Root
+          type="single"
+          defaultValue={data[0].title}
+          onValueChange={(value) => setActiveItem(value)}
+          className="col-span-12 lg:col-span-5 flex flex-col items-start gap-6"
+        >
+          {data.map((item) => (
+            <RadixAccordion.Item
+              key={item.title}
+              value={item.title}
+              className="Item rounded-2xl w-full"
+            >
+              <RadixAccordion.Trigger
+                key={item.title}
+                value={item.title}
+                asChild
+              >
+                <div className="TabsTrigger py-4 px-6 cursor-pointer rounded-xl">
+                  {item.label && (
+                    <p className="block uppercase tracking-widest">
+                      {item.label}
+                    </p>
+                  )}
+                  <h3 className="block font-semibold text-2xl">
+                    {item.title}
+                  </h3>
+                </div>
+              </RadixAccordion.Trigger>
+              <RadixAccordion.Content className='Content px-6'>
+                <p className=" block text-lg">
+                  {item.content}
+                </p>
+                <Image
+                  src={item.media?.src ?? ''}
+                  alt=""
+                  width={500}
+                  height={500}
+                  className="block lg:hidden mt-6 h-full object-cover"
+                />
+              </RadixAccordion.Content>
+            </RadixAccordion.Item>
+          ))}
+        </RadixAccordion.Root>
+        <div className="hidden lg:grid lg:col-span-7">
+          {data.map((item) => (
+            <div
+              key={item.title}
+              className={classNames(
+                "TabsContent text-lg col-start-1 row-start-1 flex flex-col gap-5 items-center justify-center transition-all ease-in-out duration-500 relative ",
+                { "opacity-100 right-0": activeItem === item.title },
+                { "opacity-0 -right-24": activeItem !== item.title }
+              )}
+            >
+              <Image
+                src={item.media?.src ?? ''}
+                alt=""
+                width={500}
+                height={500}
+                className="h-full object-cover"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+  return <></>
+}
+
+export default VTabs
