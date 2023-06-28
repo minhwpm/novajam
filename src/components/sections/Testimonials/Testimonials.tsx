79f6c76220459470ee8b8213e67a9d70ad0fd3a3
Minh Nguyen <minhhien134@gmail.com>
2023-06-28 13:46:12 +0700

    modify Testimonials

--- src/components/sections/Testimonials/Testimonials.tsx
+++ src/components/sections/Testimonials/Testimonials.tsx
@@ -4,6 +4,7 @@ import classNames from "classnames";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import Section from "@/components/elements/Section/Section";
 
 const dummyData = {
   title: "Don’t take our word for it, check how Bluebiz helps users grow their business",
@@ -87,39 +88,37 @@ const Testimonials = ( { data = dummyData }: Props) => {
   const [visibleIdx, setVisibleIdx] = useState(0);
 
   return (
-    <div className="bg-gray-200 bg-opacity-60 px-5">
-      <div className="overflow-x-hidden py-20 sm:p-5 md:p-16 xl:p-32">
-        <div className="xl:flex xl:gap-5">
-          <div className="xl:w-1/2 pr-10">
-            <h2 className="text-3xl lg:text-4xl lg:leading-[50px] font-bold mb-10">
-              {data.title}
-            </h2>
-            <div className="hidden xl:flex gap-12">
-              <ArrowGroup visibleIdx={visibleIdx} setVisibleIdx={setVisibleIdx} length={data.sections.length} />
-            </div>
-          </div>
-          <div className="xl:w-1/2 grid ">
-            {data.sections.map((item, idx) => (
-              <div
-                key={item.content} //@TODO key is too long. does it hurt performance?
-                className={classNames(
-                  "col-start-1 row-start-1 flex flex-col gap-5 justify-center p-12 shadow-xl bg-white rounded-lg transition-all ease-in-out duration-500 relative bg-[url('/images/quote-left.svg')] bg-no-repeat",
-                  { "opacity-100 right-0": visibleIdx == idx },
-                  { "opacity-0 -right-24": visibleIdx != idx }
-                )}
-              >
-                <h3 className="text-2xl font-semibold">{item.title}</h3>
-                <p className="text-lg leading-8 italic">{item.content}</p>
-                <p> - {item.source}</p>
-              </div>
-            ))}
-          </div>
-          <div className="flex xl:hidden gap-12 mt-8 justify-center">
+    <Section className="bg-gray-200 bg-opacity-60 min-h-[80vh]">
+      <div className="xl:flex xl:gap-5 overflow-hidden ">
+        <div className="xl:w-1/2 pr-10">
+          <h2 className="text-3xl lg:text-4xl lg:leading-[50px] font-bold mb-10">
+            {data.title}
+          </h2>
+          <div className="hidden xl:flex gap-12">
             <ArrowGroup visibleIdx={visibleIdx} setVisibleIdx={setVisibleIdx} length={data.sections.length} />
           </div>
         </div>
+        <div className="xl:w-1/2 grid ">
+          {data.sections.map((item, idx) => (
+            <div
+              key={item.content} //@TODO key is too long. does it hurt performance?
+              className={classNames(
+                "col-start-1 row-start-1 flex flex-col gap-5 justify-center p-12 shadow-2xl bg-white rounded-lg transition-all ease-in-out duration-500 relative bg-[url('/images/quote-left.svg')] bg-no-repeat",
+                { "opacity-100 right-0": visibleIdx == idx },
+                { "opacity-0 -right-24": visibleIdx != idx }
+              )}
+            >
+              <h3 className="text-2xl font-semibold">{item.title}</h3>
+              <p className="text-lg leading-8 italic">{item.content}</p>
+              <p> - {item.source}</p>
+            </div>
+          ))}
+        </div>
+        <div className="flex xl:hidden gap-12 mt-8 justify-center">
+          <ArrowGroup visibleIdx={visibleIdx} setVisibleIdx={setVisibleIdx} length={data.sections.length} />
+        </div>
       </div>
-    </div>
+    </Section>
   );
 };
 
