061962fcb966bf87a64dbd6a3f44a781c98ca7ba
Minh Nguyen <minhhien134@gmail.com>
2023-09-16 13:42:08 +0700

    fix TypedUseSelectorHook import from react-redux errors

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,9 +1,6 @@
-import { TypedUseSelectorHook } from "@types/react-redux";
-import { useDispatch, useSelector } from "react-redux";
+import * as ReactRedux from "react-redux";
 import { AppDispatch, RootState } from "./store";
 
 
-export const useAppDispatch  =  () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState>  = useSelector
-// export const useAppDispatch  =  () => useDispatch() as AppDispatch
-// export const useAppSelector  = useSelector as TypedUseSelectorHook<RootState>
+export const useAppDispatch  =  () => ReactRedux.useDispatch<AppDispatch>()
+export const useAppSelector: ReactRedux.TypedUseSelectorHook<RootState>  = ReactRedux.useSelector
