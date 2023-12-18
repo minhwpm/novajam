578b9ad90bd51b9a4f66c94780570d8a61f22c54
Minh Nguyen <minhhien134@gmail.com>
2023-12-18 13:27:15 +0700

    tweak PricingPlan

--- src/components/elements/PricingPlan/PricingPlan.tsx
+++ src/components/elements/PricingPlan/PricingPlan.tsx
@@ -10,21 +10,21 @@ export const PricingPlan: React.FC<{ data: PricingPlanType }> = ({ data }) => {
       className={classNames(
         "flex flex-col gap-4 items-center rounded-assets shadow-radiant",
         { "bg-white mt-16": !badge },
-        { "bg-primary-50 mt-0": badge }
+        { "bg-primary-50 mt-0 border-2 border-primary-100": badge }
       )}
     >
       {badge && (
-        <div className="w-full text-center bg-secondary-500 text-white px-3 py-2 font-bold rounded-t-assets">
+        <div className="w-full text-center bg-secondary-500 border-2 border-secondary-500 text-white px-3 py-2 font-bold rounded-t-assets tracking-wider">
           {badge}
         </div>
       )}
-      <h4 className={classNames("text-2xl font-bold",
+      <h4 className={classNames("text-2xl text-neutral-700 font-bold",
         { "mt-12": !badge },
         { "mt-8": badge },
       )}>{title}</h4>
       <div className="flex flex-col items-center">
         <div className="text-3xl font-bold">{pricing}</div>
-        <div className="text-neutral-500 text-sm">{pricingSuffix}</div>
+        <div className="text-neutral-400 text-sm tracking-wide">{pricingSuffix}</div>
       </div>
       <div>
         <Button
