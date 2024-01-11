e7951f2f2ed1327f02ed25dcb0b0bd82d8b4670e
Minh Nguyen <minhhien134@gmail.com>
2024-01-11 16:57:31 +0700

    modify

--- src/components/sections/BlogDetails/BlogDetails.tsx
+++ src/components/sections/BlogDetails/BlogDetails.tsx
@@ -12,30 +12,19 @@ export const BlogDetails: React.FC<{data: BlogType}> = ({ data }) => {
     <div>
       <Container>
         <article className="w-full my-10 flex flex-col gap-y-10">
-          <h1 className="text-heading font-heading leading-tight font-bold text-5xl mt-10 max-w-5xl">
+          <h1 className="text-heading font-heading leading-tight font-bold text-5xl mt-10 max-w-5xl text-center mx-auto">
             {title}
           </h1>
-          <div className="grid lg:grid-cols-3 gap-10">
-            <div className="lg:col-span-2">
+          <div className="flex justify-center">
+            <div className="lg:w-2/3">
               <p className="text-slate-600 text-lg prose lg:prose-lg max-w-none">
                 {summary}
               </p>
             </div>
           </div>
-          <div className="grid lg:grid-cols-3 gap-10">
-            {media && 
-              <div className="lg:col-span-2 aspect-16/9">
-                <Image  
-                  className="w-full h-full object-cover"
-                  src={media.url}
-                  alt={media.title}
-                  width={media.width}
-                  height={media.height}
-                />
-              </div>
-            }
-            <div className="lg:col-span-1 flex flex-col gap-6">
-              <div className="text-neutral-500 font-semibold">
+          <div className="flex flex-col items-center gap-10">
+            <div className="lg:w-2/3">
+              <div className="text-neutral-500 font-semibold mb-4">
                 {format(Date.parse(firstPublishedAt), 'MMMM dd, yyyy')}
               </div>
               
@@ -55,24 +44,31 @@ export const BlogDetails: React.FC<{data: BlogType}> = ({ data }) => {
                 </div>
               }
             </div>
+            {media && 
+              <div className="lg:w-2/3 aspect-16/9">
+                <Image  
+                  className="w-full h-full object-cover"
+                  src={media.url}
+                  alt={media.title}
+                  width={media.width}
+                  height={media.height}
+                />
+              </div>
+            }
           </div>
-          <div className="grid lg:grid-cols-3 gap-10 text-lg">
-            <div className="lg:col-span-2 mb-10 prose lg:prose-lg max-w-none overflow-x-hidden">
+          <div className="flex flex-col items-center gap-10 text-lg">
+            <div className="lg:w-2/3 mb-10 prose lg:prose-lg max-w-none overflow-x-hidden">
               <RichText2 data={content} style="blog" />
             </div>
-          </div>
-
-          { author && 
-            <div className="grid lg:grid-cols-3 gap-10 text-lg">
-              <div className="lg:col-span-2 mb-10">
+            { author && 
+              <div className="lg:w-2/3 text-lg mb-10">
                 <p className="text-slate-600 tracking-wider text-sm md:text-base">
                   ABOUT THE AUTHOR
                 </p>
                 <ExpertPreview data={author} layout="horizontal" />
               </div>
-            </div>
-          }
-
+            }
+          </div>
         </article>
       </Container>
       <div className="bg-primary-50 py-14">
