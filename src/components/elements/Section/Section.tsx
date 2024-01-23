bd60c453235fdb507d61ccde0c5a16e5d8a058b2
Minh Nguyen <minhhien134@gmail.com>
2024-01-23 16:44:40 +0700

    tweak

--- src/components/elements/Section/Section.tsx
+++ src/components/elements/Section/Section.tsx
@@ -35,7 +35,7 @@ export const Section: React.FC<Props> = ( { id, heading, eyebrow, summary, child
     >
       <Container>
         {eyebrow && (
-          <div className="tracking-widest text-primary-600 font-semibold text-center mx-auto mb-2">
+          <div className="tracking-widest text-primary-600 font-semibold text-center mx-auto">
             {eyebrow}
           </div>
         )}
