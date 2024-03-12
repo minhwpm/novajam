8bded28619064ca5c1270927d1223d8f6167392f
Minh Nguyen <minhhien134@gmail.com>
2024-03-12 17:01:11 +0700

    modify spacing between heading/summary with carousel

--- src/components/sections/CarouselPT/CarouselPT.tsx
+++ src/components/sections/CarouselPT/CarouselPT.tsx
@@ -14,7 +14,6 @@ import "swiper/css/pagination";
 import "@/app/css/custom-swiper.css"
 import "@/app/css/padding.css"
 
-// @TODO - modify spacing between heading/summary with carousel
 export const CarouselPT: React.FC<{ data: ContentPTType }> = ({ data }) => {
   const { eyebrow, heading, summary, content, alignment, backgroundColor, backgroundImage, darkMode } = data
   return (
@@ -28,57 +27,58 @@ export const CarouselPT: React.FC<{ data: ContentPTType }> = ({ data }) => {
       backgroundImage={backgroundImage}
       darkMode={darkMode}
     >
-      <Swiper
-        slidesPerView={1}
-        navigation={{
-          enabled: true,
-          nextEl: ".carouselPT-btn-next",
-          prevEl: ".carouselPT-btn-prev"
-        }}
-        autoplay={{
-          delay: 5000
-        }}
-        loop={true}
-        modules={[Navigation, Autoplay]}
-      >
-        {content.map((item) => (
-          <SwiperSlide key={item.id}>
-            <div className={classNames(
-              "h-full flex flex-col-reverse lg:flex-row lg:items-center gap-x-16 gap-y-5 px-5 pt-5 pb-10 md:px-10 md:pt-10 lg:px-16 lg:pt-12 lg:pb-12 rounded-assets"
+        <Swiper
+          slidesPerView={1}
+          navigation={{
+            enabled: true,
+            nextEl: ".carouselPT-btn-next",
+            prevEl: ".carouselPT-btn-prev"
+          }}
+          autoplay={{
+            delay: 5000
+          }}
+          loop={true}
+          modules={[Navigation, Autoplay]}
+        >
+          {content.map((item) => (
+            <SwiperSlide key={item.id}>
+              <div className={classNames(
+                "h-full flex flex-col-reverse lg:flex-row lg:items-center gap-x-16 gap-y-5 rounded-assets px-4 lg:px-10"
+              )}>
+              {(item.heading || item.description || item.buttons) && (
+                <div className={classNames(
+                  "flex flex-col",
+                  { "text-center": alignment === "center" },
+                  { "text-end": alignment === "reverse" }
+                )}>
+                  <TextPart data={item} alignment={alignment} darkMode={darkMode} />
+                </div>
+              )}
+              { (item.media.length > 0 || item.embeddedMediaUrl)&&  
+                <div className={classNames(
+                  { "lg:w-1/2 shrink-0 ": (item.heading || item.description) },
+                  { "w-full": (!item.heading && !item.description) },
+                )}>
+                  <FlexibleContentMediaPart data={item} alignment={alignment} />
+                </div>
+              }
+              </div>
+            </SwiperSlide>
+          ))}
+          <div className="mt-4 lg:mt-0 w-full flex justify-center gap-4">
+            <div className={classNames("carouselPT-btn-prev cursor-pointer lg:absolute lg:left-0 lg:top-1/2 lg:-translate-y-1/2 z-10 flex justify-center items-center rounded-assets w-14 h-14 bg-neutral-200 bg-opacity-20 hover:text-primary-600 hover:bg-opacity-80 transition-all duration-500 ease",
+              {"text-neutral-50": darkMode}
             )}>
-            {(item.heading || item.description || item.buttons) && (
-              <div className={classNames(
-                "flex flex-col",
-                { "text-center": alignment === "center" },
-                { "text-end": alignment === "reverse" }
-              )}>
-                <TextPart data={item} alignment={alignment} darkMode={darkMode} />
-              </div>
-            )}
-            { (item.media.length > 0 || item.embeddedMediaUrl)&&  
-              <div className={classNames(
-                { "lg:w-1/2 shrink-0 ": (item.heading || item.description) },
-                { "w-full": (!item.heading && !item.description) },
-              )}>
-                <FlexibleContentMediaPart data={item} alignment={alignment} />
-              </div>
-            }
+              <IoIosArrowBack size={35} />
             </div>
-          </SwiperSlide>
-        ))}
-        <div className="w-full flex justify-center gap-4">
-          <div className={classNames("carouselPT-btn-prev cursor-pointer lg:absolute lg:left-0 lg:top-1/2 lg:-translate-y-1/2 z-10 flex justify-center items-center rounded-assets w-14 h-14 bg-neutral-200 bg-opacity-20 hover:bg-primary-600 hover:text-neutral-100 transition-colors duration-500 ease",
-            {"text-neutral-50": darkMode}
-          )}>
-            <IoIosArrowBack size={35} />
+            <div className={classNames("carouselPT-btn-next cursor-pointer lg:absolute lg:right-0 lg:top-1/2 lg:-translate-y-1/2 z-10 flex justify-center items-center rounded-assets w-14 h-14 bg-neutral-200 bg-opacity-20 hover:text-primary-600 hover:bg-opacity-80 transition-all duration-500 ease",
+              {"text-neutral-50": darkMode}
+            )}>
+              <IoIosArrowForward size={35} />
+            </div>
           </div>
-          <div className={classNames("carouselPT-btn-next cursor-pointer lg:absolute lg:right-0 lg:top-1/2 lg:-translate-y-1/2 z-10 flex justify-center items-center rounded-assets w-14 h-14 bg-neutral-200 bg-opacity-20 hover:bg-primary-600 hover:text-neutral-100 transition-colors duration-500 ease",
-            {"text-neutral-50": darkMode}
-          )}>
-            <IoIosArrowForward size={35} />
-          </div>
-        </div>
-      </Swiper>
+        </Swiper>
+      
     </Section>
   )
 }
