25b2d30a9bfceb50aba37e58ea4dc7cfe00807d7
Minh Nguyen <minhhien134@gmail.com>
2024-03-13 13:47:39 +0700

    refactor

--- src/components/elements/PagePreview/PagePreview.tsx
+++ src/components/elements/PagePreview/PagePreview.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link"
 import { PageType } from "@/helpers/types"
 import { MediaItem } from "../MediaItem/MediaItem"
+import { useContext } from "react"
+import { DarkModeContext } from "@/components/sections/ContentList/ContentList";
+import classNames from "classnames";
 
 export const PagePreview: React.FC<{
   data: PageType
@@ -10,16 +13,19 @@ export const PagePreview: React.FC<{
   layout = "vertical"
 }) => {
   const { title, url, metaTitle, metaImage } = data
+  const darkMode = useContext(DarkModeContext);
   if (layout === "horizontal") {
     return (
-      <div className="rounded-assets bg-white bg-opacity-80 flex gap-5">
+      <div className="rounded-assets flex gap-5">
         <div className="basis-1/3 flex-1">
           <Link href={url}>
             <MediaItem data={metaImage} aspectRatio="square" />
           </Link>
         </div>
         <div className="basis-2/3 flex-1 py-4 pr-4 lg:py-6 lg:pr-6">
-          <h4 className="text-xl font-heading file:font-semibold">
+          <h4 className={classNames("text-xl font-heading file:font-semibold",
+            { "text-neutral-50": darkMode }
+          )}>
             <Link href={url}>
               {metaTitle ?? title}
             </Link>
@@ -29,13 +35,18 @@ export const PagePreview: React.FC<{
     )
   }
   return (
-    <div className="rounded-assets flex flex-col bg-white bg-opacity-80">
-      <Link href={url} >
+    <div className="rounded-assets flex flex-col">
+      <Link href={url}>
         <MediaItem data={metaImage} aspectRatio="4/3" />
-        <h4 className="mt-4 text-xl font-heading font-semibold text-center pb-4 lg:pb-6">
+        <h4
+          className={classNames(
+            "mt-4 text-xl font-heading font-semibold text-center pb-4 lg:pb-6",
+            { "text-neutral-50": darkMode }
+          )}
+        >
           {metaTitle ?? title}
         </h4>
       </Link>
     </div>
-  )
+  );
 }
