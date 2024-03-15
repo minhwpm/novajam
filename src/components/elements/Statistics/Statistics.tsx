f0d92bbd100de85e204ece406c0e68ead19556f1
Minh Nguyen <minhhien134@gmail.com>
2024-03-15 11:11:15 +0700

    darkMode for Statistics

--- src/components/elements/Statistics/Statistics.tsx
+++ src/components/elements/Statistics/Statistics.tsx
@@ -2,9 +2,12 @@
 import classNames from "classnames";
 import { useInView } from "react-hook-inview";
 import { StatisticsType } from "@/helpers/types"
+import { useContext } from "react";
+import { DarkModeContext } from "@/components/sections/ContentList/ContentList";
 
 export const Statistics: React.FC<{ data: StatisticsType, index: number }> = ({ data, index }) => {
   const { number, text } = data
+  const darkMode = useContext(DarkModeContext);
   const [ref, isIntersecting] = useInView({
     threshold: 1,
     unobserveOnEnter: true
@@ -14,9 +17,10 @@ export const Statistics: React.FC<{ data: StatisticsType, index: number }> = ({ 
     <div 
       ref={ref}
       className={classNames(
-        "flex flex-col items-center gap-3 p-4 rounded-assets",
+        "flex flex-col items-center gap-3 p-4 lg:p-6 bg-white rounded-assets",
         { "perspective-2500 backface-hidden -rotate-y-90": !isIntersecting },
         { "perspective-none backface-hidden rotate-y-0 transition-transform ease duration-1000 " : isIntersecting },
+        { "bg-opacity-5": darkMode }
       )}
       style={{ 
         transitionDelay: `${(index + 1) * 0.2}s`
@@ -25,7 +29,10 @@ export const Statistics: React.FC<{ data: StatisticsType, index: number }> = ({ 
       <div className="font-heading text-4xl md:text-6xl xl:text-7xl 2xl:text-8xl font-bold text-primary-600 text-center">
         {number}
       </div>
-      <div className="text-neutral-500 font-heading font-semibold tracking-wide text-center md:text-lg xl:text-xl max-w-[150px]">
+      <div className={classNames("font-heading font-semibold tracking-wide text-center md:text-lg xl:text-xl max-w-[150px]",
+        {"text-neutral-600": !darkMode},
+        {"text-neutral-300": darkMode},
+      )}>
         {text}
       </div>
     </div>
