7abd78c88c33a161fd61e1e0c7b2121af8c10652
Minh Nguyen <minhhien134@gmail.com>
2024-03-27 10:13:02 +0700

    refactor SelectField

--- src/components/sections/InquiryForm/SelectField.tsx
+++ src/components/sections/InquiryForm/SelectField.tsx
@@ -1,4 +1,5 @@
-import { Select } from "@/components/elements/Select/Select";
+import ReactSelect from "react-select";
+import { useController } from "react-hook-form";
 import { FormFieldType } from "@/helpers/types";
 import { Control, UseFormRegister } from "react-hook-form/dist/types/form";
 import { FormValues } from "./InquiryForm";
@@ -8,16 +9,28 @@ export const SelectField: React.FC<{
   control: Control<FormValues, any>;
   register: UseFormRegister<FormValues>;
 }> = ({ data, control, register }) => {
+  const { field } = useController({
+    name: data.label,
+    control,
+    rules: { required: data.required },
+  });
   return (
-    <Select
+    <ReactSelect
       className="rounded-assets w-full"
-      control={control}
       options={data.options.map(option => {
         return {
           label: option,
           value: option
         }
       })}
+      styles={{
+        control: (baseStyles) => ({
+          ...baseStyles,
+          padding: "8px 6px",
+          borderColor: "rgb(212 212 212)",
+          borderRadius: "var(--border-radius-assets)",
+        }),
+      }}
       placeholder={(
         <div className="text-neutral-500">
           {data.label + (data.required ? "*" : "")}
@@ -26,6 +39,9 @@ export const SelectField: React.FC<{
       {...register(data.label, {
         required: data.required,
       })}
+      onChange={(value) => {
+        field.onChange(value)
+      }}
     />
   )
 }
