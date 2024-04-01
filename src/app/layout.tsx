bdc8ad8e4dacd61f6655865be7ade8a663c26f47
Minh Nguyen <minhhien134@gmail.com>
2024-04-01 15:20:00 +0700

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Metadata } from "next";
 import "@/app/css/globals.css";
-
-//@TODO redundant?
-export const metadata: Metadata = {
-  title: "NovaJAM",
-  description: "All-in-One JAMstack Web Solution",
-  openGraph: {
-    images: [
-      {
-        url: "/bluebiz_square.webp",
-      },
-    ],
-  },
-};
 
 export default async function Layout({
   children,
