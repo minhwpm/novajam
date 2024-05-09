e634fb667c4e63b0d50551718d582c7b0c669fac
Minh Nguyen <minhhien134@gmail.com>
2024-05-09 09:23:47 +0700

    add Loading in dynamic

---
+++ src/app/(dynamic)/[...slug]/loading.tsx
@@ -0,0 +1,9 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+export default function Loading() {
+  return (
+    <div className="mx-auto flex flex-col items-center justify-center min-h-screen">
+      <AiOutlineLoading3Quarters className="animate-spin text-primary-600" size={40} />
+    </div>
+  );
+}
