3000d0eab354e897030b5c794c11d71efef2615c
Minh Nguyen <minhhien134@gmail.com>
2024-05-11 09:26:36 +0700

    modify

--- src/components/elements/SNS/SNS.tsx
+++ src/components/elements/SNS/SNS.tsx
@@ -5,45 +5,100 @@ import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram, FaYoutube, FaLinkedinIn } from "react-icons/fa";
 import Link from "next/link";
 
-export const SNS: React.FC<{ data: SNSType, variant?: 'standard' | 'alternate'}> = ({ data, variant }) => {
-  const { linkedInUrl, facebookUrl, twitterUrl, youtubeUrl, instagramUrl } = data
+export const SNS: React.FC<{
+  data: SNSType;
+  variant?: "standard" | "alternate";
+  darkMode: boolean;
+}> = ({ data, variant, darkMode = false }) => {
+  const { linkedInUrl, facebookUrl, twitterUrl, youtubeUrl, instagramUrl } =
+    data;
   return (
-    <div className="flex gap-5 items-center py-3">
-      {linkedInUrl && 
-        <Link href={linkedInUrl} className={classNames(
-          { "group w-10 h-10 rounded-full flex justify-center items-center bg-neutral-100 hover:bg-neutral-200 transition-colors duration-300 ease-in-out": variant === "alternate" }
-        )}>
-          <FaLinkedinIn className={classNames("text-primary-600 group-hover:text-primary-600 transition-colors duration-300 ease-in-out")} />
+    <div className={classNames("flex gap-5 items-center py-3")}>
+      {linkedInUrl && (
+        <Link
+          href={linkedInUrl}
+          className={classNames({
+            "group w-10 h-10 rounded-full flex justify-center items-center bg-neutral-100 hover:bg-neutral-200 transition-colors duration-300 ease-in-out":
+              variant === "alternate",
+          })}
+        >
+          <FaLinkedinIn
+            className={classNames(
+              "transition-colors duration-300 ease-in-out",
+              { "text-primary-600 group-hover:text-primary-500": !darkMode },
+              { "text-neutral-100 group-hover:text-neutral-50": darkMode }
+            )}
+          />
         </Link>
-      }
-      {facebookUrl && 
-        <Link href={facebookUrl} className={classNames(
-          { "group w-10 h-10 rounded-full flex justify-center items-center bg-neutral-100 hover:bg-neutral-200": variant === "alternate" }
-        )}>
-          <RiFacebookFill className={classNames("text-primary-600 group-hover:text-primary-600 transition-colors duration-300 ease-in-out")} />
+      )}
+      {facebookUrl && (
+        <Link
+          href={facebookUrl}
+          className={classNames({
+            "group w-10 h-10 rounded-full flex justify-center items-center bg-neutral-100 hover:bg-neutral-200":
+              variant === "alternate",
+          })}
+        >
+          <RiFacebookFill
+            className={classNames(
+              "transition-colors duration-300 ease-in-out",
+              { "text-primary-600 group-hover:text-primary-500": !darkMode },
+              { "text-neutral-100 group-hover:text-neutral-50": darkMode }
+            )}
+          />
         </Link>
-      }
-      {twitterUrl && 
-        <Link href={twitterUrl} className={classNames(
-          { "group w-10 h-10 rounded-full flex justify-center items-center bg-neutral-100 hover:bg-neutral-200": variant === "alternate" }
-        )}>
-          <FaXTwitter className={classNames("text-primary-600 group-hover:text-primary-600 transition-colors duration-300 ease-in-out")} />
+      )}
+      {twitterUrl && (
+        <Link
+          href={twitterUrl}
+          className={classNames({
+            "group w-10 h-10 rounded-full flex justify-center items-center bg-neutral-100 hover:bg-neutral-200":
+              variant === "alternate",
+          })}
+        >
+          <FaXTwitter
+            className={classNames(
+              "transition-colors duration-300 ease-in-out",
+              { "text-primary-600 group-hover:text-primary-500": !darkMode },
+              { "text-neutral-100 group-hover:text-neutral-50": darkMode }
+            )}
+          />
         </Link>
-      }
-      {youtubeUrl && 
-        <Link href={youtubeUrl} className={classNames(
-          { "group w-10 h-10 rounded-full flex justify-center items-center bg-neutral-100 hover:bg-neutral-200": variant === "alternate" }
-        )}>
-          <FaYoutube className={classNames("text-primary-600 group-hover:text-primary-600 transition-colors duration-300 ease-in-out")} />
+      )}
+      {youtubeUrl && (
+        <Link
+          href={youtubeUrl}
+          className={classNames({
+            "group w-10 h-10 rounded-full flex justify-center items-center bg-neutral-100 hover:bg-neutral-200":
+              variant === "alternate",
+          })}
+        >
+          <FaYoutube
+            className={classNames(
+              "transition-colors duration-300 ease-in-out",
+              { "text-primary-600 group-hover:text-primary-500": !darkMode },
+              { "text-neutral-100 group-hover:text-neutral-50": darkMode }
+            )}
+          />
         </Link>
-      }
-      {instagramUrl && 
-        <Link href={instagramUrl} className={classNames(
-          { "group w-10 h-10 rounded-full flex justify-center items-center bg-neutral-100 hover:bg-neutral-200": variant === "alternate" }
-        )}>
-          <FaInstagram className={classNames("text-primary-600 group-hover:text-primary-600 transition-colors duration-300 ease-in-out")} />
+      )}
+      {instagramUrl && (
+        <Link
+          href={instagramUrl}
+          className={classNames({
+            "group w-10 h-10 rounded-full flex justify-center items-center bg-neutral-100 hover:bg-neutral-200":
+              variant === "alternate",
+          })}
+        >
+          <FaInstagram
+            className={classNames(
+              "transition-colors duration-300 ease-in-out",
+              { "text-primary-600 group-hover:text-primary-500": !darkMode },
+              { "text-neutral-100 group-hover:text-neutral-50": darkMode }
+            )}
+          />
         </Link>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
--- src/components/sections/Footer/Footer.tsx
+++ src/components/sections/Footer/Footer.tsx
@@ -28,8 +28,8 @@ export const Footer: React.FC<Props> = ({ data }) => {
           : {}
       }
     >
-      <div className="container mx-auto px-4 grid grid-cols-12 gap-x-5 gap-y-10">
-        <div className="col-span-12 lg:col-span-6 xl:col-span-4 flex flex-col items-center lg:items-start">
+      <div className="container mx-auto px-4 flex flex-wrap gap-x-5 gap-y-10">
+        <div className="w-full lg:w-1/2 xl:w-1/3 flex flex-col items-center lg:items-start">
           {logo?.url && (
             <Link href={logoRedirect ?? "/"}>
               <Image
@@ -43,50 +43,53 @@ export const Footer: React.FC<Props> = ({ data }) => {
           )}
           <p
             className={classNames(
+              "mt-4",
               { "text-neutral-500": !darkMode },
               { "text-neutral-200": darkMode }
             )}
           >
             {copyright}
           </p>
-          <div className="flex gap-2 mt-5">
-            {sns && <SNS data={sns} variant="alternate" />}
+          <div className="flex gap-2 mt-2">
+            {sns && <SNS darkMode={darkMode} data={sns} />}
           </div>
         </div>
-        {menu.map((section, idx) => (
-          <div
-            key={idx}
-            className={classNames(
-              "col-span-6 lg:col-span-3 xl:col-span-2 flex flex-col gap-2",
-              { "xl:col-start-10": menu.length === 1 },
-              { "text-neutral-100": darkMode }
-            )}
-          >
+        <div className="grow flex flex-wrap gap-8 justify-center lg:justify-end">
+          {menu.map((section, idx) => (
             <div
+              key={idx}
               className={classNames(
-                "font-semibold mb-1",
-                { "text-neutral-600": !darkMode },
-                { "text-neutral-200": darkMode }
+                "basis-40 flex flex-col gap-2",
+                { "xl:col-start-10": menu.length === 1 },
+                { "text-neutral-100": darkMode }
               )}
             >
-              {section.title}
-            </div>
-            {section.links.map((link) => (
-              <Link
-                key={link.text}
-                href={link.url}
+              <div
                 className={classNames(
-                  "self-start select-none underline-hover-effect",
-                  { "before:bg-primary-500": !darkMode },
-                  { "before:bg-primary-100": darkMode }
+                  "font-semibold mb-1",
+                  { "text-neutral-600": !darkMode },
+                  { "text-neutral-200": darkMode }
                 )}
-                target={link.openNewTab ? "_blank" : "_self"}
               >
-                {link.text}
-              </Link>
-            ))}
-          </div>
-        ))}
+                {section.title}
+              </div>
+              {section.links.map((link) => (
+                <Link
+                  key={link.text}
+                  href={link.url}
+                  className={classNames(
+                    "self-start select-none underline-hover-effect",
+                    { "before:bg-primary-500": !darkMode },
+                    { "before:bg-primary-100": darkMode }
+                  )}
+                  target={link.openNewTab ? "_blank" : "_self"}
+                >
+                  {link.text}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </footer>
   );
