db6d48d8cd36679b751f0887dd9753b18c3836fd
Minh Nguyen <minhhien134@gmail.com>
2024-05-14 12:03:42 +0700

    modify

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div className="mx-auto p-4 flex flex-col max-w-3xl justify-center items-center pt-32 pb-20 min-h-screen">
-      <div className="font-bold text-7xl lg:text-9xl text-primary-600 mb-5">404</div>
-      <Image src="/404.png" alt="404 Page Not Found" width="500" height="500"/>
+    <div className="mx-auto p-4 flex flex-col max-w-3xl justify-center items-center min-h-screen">
+      <Image src="/404.png" alt="404 Page Not Found" className="w-full max-w-sm" width="300" height="300"/>
+      <div className="font-bold text-4xl lg:text-6xl text-center text-primary-600 mb-5">Page Not Found</div>
       <div className="text-neutral-500 lg:text-lg text-center">
         <p>We’re sorry, the page you requested could not be found.</p>
         <p>Please go back to the homepage.</p>
