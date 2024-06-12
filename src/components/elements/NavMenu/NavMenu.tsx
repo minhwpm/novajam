93ac6f0feb8b9d3d9260e4152229c73897c136cd
Minh Nguyen <minhhien134@gmail.com>
2024-06-12 08:49:35 +0700

    modify

--- src/components/elements/NavMenu/NavMenu.tsx
+++ src/components/elements/NavMenu/NavMenu.tsx
@@ -15,39 +15,45 @@ const NavMenu: React.FC<{
 }> = ({ menu, appearanceVariant }) => {
   return (
     <NavigationMenu.Root
-      className={classNames(
-        "NavMenu hidden lg:flex justify-end"
-      )}
+      className={classNames("NavMenu hidden lg:flex justify-end")}
     >
       <NavigationMenu.List
         className={classNames(
           "flex justify-center items-start px-5 list-none m-0"
-          // "gap-x-10"
         )}
       >
-        {menu.map((item) => item && (
-          <NavigationMenu.Item
-            key={item.id}
-            className={classNames(
-              "text-smd",
-              { relative: "menu" in item && item.appearanceVariant === "dropdown" }
-            )}
-          >
-            {item.contentType === "link" && (
-              <div className="py-7 px-5">
-                <NavLinkItem
-                  key={item.id}
-                  href={item.url}
-                  variant="underlined"
-                  openNewTab={item.openNewTab}
-                >
-                  {item.text}
-                </NavLinkItem>
-              </div>
-            )}
-            {item.contentType === "submenu" && <Submenu key={item.id} data={item} appearanceVariant={appearanceVariant} />}
-          </NavigationMenu.Item>
-        ))}
+        {menu.map(
+          (item) =>
+            item && (
+              <NavigationMenu.Item
+                key={item.id}
+                className={classNames("text-smd", {
+                  relative:
+                    "menu" in item && item.appearanceVariant === "dropdown",
+                })}
+              >
+                {item.contentType === "link" && (
+                  <div className="py-7 px-5">
+                    <NavLinkItem
+                      key={item.id}
+                      href={item.url}
+                      variant="underlined"
+                      openNewTab={item.openNewTab}
+                    >
+                      {item.text}
+                    </NavLinkItem>
+                  </div>
+                )}
+                {item.contentType === "submenu" && (
+                  <Submenu
+                    key={item.id}
+                    data={item}
+                    appearanceVariant={appearanceVariant}
+                  />
+                )}
+              </NavigationMenu.Item>
+            )
+        )}
       </NavigationMenu.List>
     </NavigationMenu.Root>
   );
