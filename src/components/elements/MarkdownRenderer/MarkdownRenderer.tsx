d4647f37efbf5182678de41630791c2b5f79125c
Minh Nguyen <minhhien134@gmail.com>
2024-08-28 16:14:30 +0700

    modify

--- src/components/elements/MarkdownRenderer/MarkdownRenderer.tsx
+++ src/components/elements/MarkdownRenderer/MarkdownRenderer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
-export const MarkdownRenderer: React.FC<{content: string, className: string}> = ({ content, className }) => {
+export const MarkdownRenderer: React.FC<{content: string, className?: string}> = ({ content, className }) => {
   return (
     <ReactMarkdown className={className}>{content}</ReactMarkdown>
   );
