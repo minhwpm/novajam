9f7efc055047be2d27259a8f536fc3da2b07b917
Minh Nguyen <minhhien134@gmail.com>
2024-09-26 13:30:30 +0700

    modify font collection

--- src/helpers/fonts/index.ts
+++ src/helpers/fonts/index.ts
@@ -9,118 +9,120 @@ import {
   Raleway,
   Inter,
   Lora,
-  Rubik,
   Lato,
   Merriweather,
-  Inria_Serif,
-  PT_Serif,
   Oswald,
   Cormorant_Garamond,
+  Nunito,
+  Nunito_Sans,
+  Noto_Sans,
+  Quicksand,
 } from 'next/font/google';
 
-export const Poppins_Font = Poppins({
-  subsets: ['latin'],
+const Poppins_Font = Poppins({
+  subsets: ['latin', 'latin-ext'],
   display: 'swap',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
   variable: '--font-heading',
 });
 
-export const Open_Sans_Font = Open_Sans({
-  subsets: ['latin'],
+const Open_Sans_Font = Open_Sans({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
   display: 'swap',
   weight: ['300', '400', '500', '600', '700', '800'],
-  variable: '--font-heading',
 });
 
-export const Montserrat_Font = Montserrat({
-  subsets: ['latin'],
+const Montserrat_Font = Montserrat({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
   display: 'swap',
   variable: '--font-heading',
 });
 
-export const Libre_Franklin_Font = Libre_Franklin({
-  subsets: ['latin'],
+const Libre_Franklin_Font = Libre_Franklin({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
   display: 'swap',
 });
 
-export const Roboto_Font = Roboto({
-  subsets: ['latin'],
+const Roboto_Font = Roboto({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
   display: 'swap',
   weight: ['100', '300', '400', '500', '700', '900'],
+});
+
+const Raleway_Font = Raleway({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
   variable: '--font-heading',
 });
 
-export const Raleway_Font = Raleway({
-  subsets: ['latin'],
+const Inter_Font = Inter({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
   display: 'swap',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-heading',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
 });
 
-export const Inter_Font = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-});
-
-export const Lato_Font = Lato({
-  subsets: ['latin'],
+const Lato_Font = Lato({
+  subsets: ['latin', 'latin-ext'],
   display: 'swap',
   weight: ['100', '300', '400', '700', '900'],
-  variable: '--font-heading',
 });
 
-export const Inria_Serif_Font = Inria_Serif({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['300', '400', '700'],
-  variable: '--font-heading',
-});
-
-export const Playfair_Display_Font = Playfair_Display({
-  subsets: ['latin'],
+const Playfair_Display_Font = Playfair_Display({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
   display: 'swap',
   weight: ['400', '600'],
   variable: '--font-heading',
 });
 
-export const Lora_Font = Lora({
-  subsets: ['latin'],
+const Lora_Font = Lora({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
   display: 'swap',
   weight: ['400', '600'],
   variable: '--font-heading',
 });
 
-export const Rubik_Font = Rubik({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-heading',
-});
-
-export const Merriweather_Font = Merriweather({
-  subsets: ['latin'],
+const Merriweather_Font = Merriweather({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
   display: 'swap',
   weight: ['300', '400', '700'],
   variable: '--font-heading',
 });
 
-export const PT_Serif_Font = PT_Serif({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400', '700'],
-  variable: '--font-heading',
-});
-
-export const Oswald_Font = Oswald({
-  subsets: ['latin'],
+const Oswald_Font = Oswald({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
   display: 'swap',
   weight: ['200', '300', '600', '700'],
   variable: '--font-heading',
 });
 
-export const Cormorant_Garamond_Font = Cormorant_Garamond({
-  subsets: ['latin'],
+const Cormorant_Garamond_Font = Cormorant_Garamond({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
+  display: 'swap',
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-heading',
+});
+
+const Nunito_Font = Nunito({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
+  display: 'swap',
+  weight: ['200', '300', '400', '500', '600', '700', '800'],
+});
+
+const Nunito_Sans_Font = Nunito_Sans({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
+  display: 'swap',
+  weight: ['200', '300', '400', '500', '600', '700', '800'],
+});
+
+const Noto_Sans_Font = Noto_Sans({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
+  display: 'swap',
+  weight: ['200', '300', '400', '500', '600', '700', '800'],
+});
+
+const Quicksand_Font = Quicksand({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
   display: 'swap',
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-heading',
@@ -131,27 +133,28 @@ export const generateFontClassnames = (
   fontHeading: string | null,
 ) =>
   classNames({
-    [Poppins_Font.className]: fontMain === 'Poppins',
-    [Poppins_Font.variable]: fontHeading === 'Poppins',
-    [Montserrat_Font.className]: fontMain === 'Montserrat',
-    [Montserrat_Font.variable]: fontHeading === 'Montserrat',
-    [Open_Sans_Font.className]: fontMain === 'Open Sans',
-    [Open_Sans_Font.variable]: fontHeading === 'Open Sans',
-    [Raleway_Font.className]: fontMain === 'Raleway',
-    [Raleway_Font.variable]: fontHeading === 'Raleway',
+    // 12 main fonts
     [Inter_Font.className]: fontMain === 'Inter',
-    [Roboto_Font.className]: fontMain === 'Roboto',
-    [Roboto_Font.variable]: fontHeading === 'Roboto',
     [Lato_Font.className]: fontMain === 'Lato',
-    [Lato_Font.variable]: fontHeading === 'Lato',
-    [Inria_Serif_Font.className]: fontMain === 'Inria Serif',
-    [Inria_Serif_Font.variable]: fontHeading === 'Inria Serif',
     [Libre_Franklin_Font.className]: fontMain === 'Libre Franklin',
-    [Oswald_Font.className]: fontMain === 'Oswald',
+    [Lora_Font.className]: fontMain === 'Lora',
+    [Merriweather_Font.className]: fontHeading === 'Merriweather',
+    [Montserrat_Font.className]: fontMain === 'Montserrat',
+    [Noto_Sans_Font.className]: fontMain === 'Noto Sans',
+    [Nunito_Font.className]: fontMain === 'Nunito',
+    [Nunito_Sans_Font.className]: fontMain === 'Nunito Sans',
+    [Open_Sans_Font.className]: fontMain === 'Open Sans',
+    [Poppins_Font.className]: fontMain === 'Poppins',
+    [Roboto_Font.className]: fontMain === 'Roboto',
+
+    // 9 heading fonts
+    [Cormorant_Garamond_Font.variable]: fontHeading === 'Cormorant Garamond',
+    [Lora_Font.variable]: fontHeading === 'Lora',
+    [Merriweather_Font.variable]: fontHeading === 'Merriweather',
+    [Montserrat_Font.variable]: fontHeading === 'Montserrat',
     [Oswald_Font.variable]: fontHeading === 'Oswald',
     [Playfair_Display_Font.variable]: fontHeading === 'Playfair Display',
-    [Lora_Font.variable]: fontHeading === 'Lora',
-    [Rubik_Font.variable]: fontHeading === 'Rubik',
-    [Merriweather_Font.variable]: fontHeading === 'Merriweather',
-    [Cormorant_Garamond_Font.variable]: fontHeading === 'Cormorant Garamond',
+    [Poppins_Font.variable]: fontHeading === 'Poppins',
+    [Raleway_Font.variable]: fontHeading === 'Raleway',
+    [Quicksand_Font.variable]: fontHeading === 'Quicksand',
   });
