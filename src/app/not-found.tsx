3d241a891d418cf6e11e4c56e449ff256f28b383
Minh Nguyen <minhhien134@gmail.com>
2024-10-17 12:38:20 +0700

    modify 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 
 export default function NotFound() {
   return (
-    <div className="container flex flex-wrap-reverse min-h-fit items-center justify-center">
+    <section className="container flex flex-wrap-reverse min-h-fit items-center justify-center">
       <div className="w-full lg:w-1/2 py-10">
-        <div className="font-bold text-4xl lg:text-6xl text-primary-600 mb-5">
+        <div className="font-bold text-4xl lg:text-6xl text-primary-600 mb-6 lg:mb-8 dark:text-primary-500">
           Page Not Found
         </div>
-        <div className="text-slate-500 lg:text-lg">
+        <div className="text-slate-600 dark:text-slate-300 lg:text-lg">
           <p>We’re sorry, the page you requested could not be found.</p>
           <p>Please go back to the homepage.</p>
         </div>
@@ -27,13 +27,13 @@ export default function NotFound() {
       </div>
       <div className="w-1/2">
         <Image
-          src="/404.png"
+          src="/404.webp"
           alt="404 Page Not Found"
           className="w-full"
           width="300"
           height="300"
         />
       </div>
-    </div>
+    </section>
   );
 }
